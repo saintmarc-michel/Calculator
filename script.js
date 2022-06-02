67a3510dc913aeee0b211e44c657a8d670a6bb8f
Michel Saint Marc <85990861+saintmarc-michel@users.noreply.github.com>
2022-06-02 15:59:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,128 @@
-const res = document.getElementById("result");
+// const res = document.getElementById("result");
 
 
-function calculate(value){
+// function calculate(value){
 
-    const calculatedValue = eval(value || null);
+//     const calculatedValue = eval(value || null);
 
-    if (isNaN(calculatedValue)) {
-        res.value = "Can't divide 0 with 0";
-        setTimeout(() => {
-            res.value ="";
-        }, 1300);
-    } else{
-        res.value = calculatedValue;
-    }
+//     if (isNaN(calculatedValue)) {
+//         res.value = "";
+//         setTimeout(() => {
+//             res.value = "";
+//         }, 1300);
+//     } else{
+//         res.value = calculatedValue;
+//     }
 
+// }
+
+// function liveScreen(enteredValue){
+
+//     if (!res.value){
+//         res.value = "";
+//     }
+//     res.value += enteredValue;
+
+// }
+
+// document.addEventListener("keydown", keyboardInputHandler);
+
+// function keyboardInputHandler(e) {
+//     e.preventDefault();
+// }
+
+// function eraseNum() {
+
+//    let res1 = res.value 
+//     let res2 = res1.slice(0, -1);
+
+//     document.getElementById("result").value= res2    
+// }
+
+
+
+
+
+
+function getHistory(){
+	return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+	document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+	return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output-value").innerText=num;
+	}
+	else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}	
 }
 
-
-// Displays entered value on screen.
-function liveScreen(enteredValue){
-
-    if (!res.value){
-        res.value = "";
-    }
-    res.value += enteredValue;
+function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
+	var n = Number(num);
+	var value = n.toLocaleString("en");
+	return value;
 }
 
-//adding event handler on the document to handle keyboard inputs
-document.addEventListener("keydown", keyboardInputHandler);
-
-//function to handle keyboard inputs
-function keyboardInputHandler(e) {
-    e.preventDefault();
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
 }
 
-function eraseNum() {
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="clear"){
+			printHistory("");
+			printOutput("");
+		}
+		else if(this.id=="backspace"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){//if output has a value
+				output= output.substr(0,output.length-1);
+				printOutput(output);
+			}
+		}
+		else{
+			var output=getOutput();
+			var history=getHistory();
+			if(output==""&&history!=""){
+				if(isNaN(history[history.length-1])){
+					history= history.substr(0,history.length-1);
+				}
+			}
+			if(output!="" || history!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				history=history+output;
+				if(this.id=="="){
+					var result=eval(history);
+					printOutput(result);
+					printHistory("");
+				}
+				else{
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
+		}
+		
+	});
+}
 
-   let res1 = res.value 
-    let res2 = res1.slice(0, -1);
-
-    document.getElementById("result").value= res2
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		var output=reverseNumberFormat(getOutput());
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
 }
